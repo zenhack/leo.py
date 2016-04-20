4118b190e64d20fd0f75a9af21b771fb7dfda487
Ian Denhardt <ian@zenhack.net>
2016-04-20 03:22:41 -0400

    Swap dependency on compat23 out for six.

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(name='leo',
-      version='0.1',
+      version='0.2',
       author='Ian Denhardt',
       author_email='ian@zenhack.net',
       url='https://gitlab.com/isd/leo.py',
       scripts=['leo'],
       install_requires=[
-          'compat23',
+          'six',
           'lxml',
           'requests',
           'tabulate',
